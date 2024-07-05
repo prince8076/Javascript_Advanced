f0a23ed66a9e5d75641f98960b2291b80c002d22
prince8076 <rahulsingh00c@gmail.com>
2024-07-05 11:19:40 +0530

    done

--- variables.js
+++ variables.js
@@ -2,6 +2,7 @@ const accoundId = 123456789;
 let accooundEmail = "rahulsingh00C@gmail.com";
 var accountPassword = "12345";
 accountCity = "Mumbai";
+let accountState;
 
 // accoundId = 2 // not allowed because it is a constant
 accooundEmail = "prince@gmail.com";
@@ -13,4 +14,4 @@ prefer not to use var
 because of issue in block scope and fuctional scope
 */
 
-console.table([accoundId, accooundEmail, accountPassword, accountCity]);
+console.table([accoundId, accooundEmail, accountPassword, accountCity, accountState]);
