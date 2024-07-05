77b1ab7e5102badd4b4427a6234078f802c477d1
prince8076 <rahulsingh00c@gmail.com>
2024-07-05 12:07:18 +0530

    done

---
+++ conversionOperation.js
@@ -0,0 +1,36 @@
+// let score = '100abc';
+let score = null;
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumver = Number(score);
+// console.log(typeof valueInNumver);
+// console.log(valueInNumver); // it will give NaN because it is not a number but the type of  valueInNumver is Number 
+
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = [];
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1=> true; 0 => false;
+// "" => false; " " => true;
+// null => false; undefined => false;
+// NaN => false;
+// {} => true; [] => true;
+// 'prince' => true;
+
+
+let someNumber = 10;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
