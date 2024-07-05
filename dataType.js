d7ac92291c34a6b34da1bc98a2811a3af1548bf7
prince8076 <rahulsingh00c@gmail.com>
2024-07-05 11:28:43 +0530

    done

---
+++ dataType.js
@@ -0,0 +1,12 @@
+" use strict"; // treat all JS code as a newer version
+
+// alert("Hello World"); // we are using nodejs, not browser 
+
+console.log(3 +
+    4
+) // code readability and maintainability is the key to success in JS 
+
+console.log(
+    "Hello" +
+    " World"
+)
