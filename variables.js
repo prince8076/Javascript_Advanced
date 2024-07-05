c478d58ccd11f285b61a128c0150ddb5c449826a
prince8076 <rahulsingh00c@gmail.com>
2024-07-05 11:18:07 +0530

    done

--- variables.js
+++ variables.js
@@ -4,4 +4,13 @@ var accountPassword = "12345";
 accountCity = "Mumbai";
 
 // accoundId = 2 // not allowed because it is a constant
+accooundEmail = "prince@gmail.com";
+accountPassword = "123444444";
+accountCity = "Delhi";
 console.log(accoundId);
+/*
+prefer not to use var 
+because of issue in block scope and fuctional scope
+*/
+
+console.table([accoundId, accooundEmail, accountPassword, accountCity]);
