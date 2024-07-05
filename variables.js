edea1572d0a8f5ac9e3a2215946a499500506a7b
prince8076 <rahulsingh00c@gmail.com>
2024-07-05 11:12:12 +0530

    done

--- variables.js
+++ variables.js
@@ -2,3 +2,6 @@ const accoundId = 123456789;
 let accooundEmail = "rahulsingh00C@gmail.com";
 var accountPassword = "12345";
 accountCity = "Mumbai";
+
+// accoundId = 2 // not allowed because it is a constant
+console.log(accoundId);
