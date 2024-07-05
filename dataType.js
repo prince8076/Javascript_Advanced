08a7e1101c14b5912ae988df663d6399046db386
prince8076 <rahulsingh00c@gmail.com>
2024-07-05 11:36:43 +0530

    done

--- dataType.js
+++ dataType.js
@@ -10,3 +10,24 @@ console.log(
     "Hello" +
     " World"
 )
+
+let name = "Rahul";  // string
+let age = 25;  // number
+let isMarried = true;  // boolean
+let height = 1.8;  // float
+
+// premitive data types
+
+// number  => 2 to power 53
+// bigint => 2 to power 64
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undfined =>  value is not assign
+// symbol => unique identifier
+
+
+// object
+
+console.log(typeof null); // it will give object type
+console.log(typeof undefined); // it will give undefined type
